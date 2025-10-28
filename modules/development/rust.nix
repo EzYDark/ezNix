710ab877687bdaaea001d36dc9c66380577f9ff0
EzYDark <ezydark@protonmail.com>
2025-10-28 18:23:54 +0100

    Little Rust lang fixes

--- modules/development/rust.nix
+++ modules/development/rust.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 let
   sccacheBin = "${pkgs.sccache}/bin/sccache";
 in {
@@ -9,16 +9,4 @@ in {
   ];
 
   environment.sessionVariables.RUSTC_WRAPPER = sccacheBin;
-
-  programs.fish = lib.mkIf config.programs.fish.enable {
-    interactiveShellInit = lib.mkAfter ''
-      set -gx SCCACHE_DIR $HOME/.cache/sccache
-      set -gx SCCACHE_CACHE_SIZE 10G
-      set -gx RUSTC_WRAPPER ${sccacheBin}
-    '';
-  };
-
-  systemd.tmpfiles.rules = [
-    "d /home/%u/.cache/sccache 0700 %u %u -"
-  ];
 }
